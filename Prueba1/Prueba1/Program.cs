b850bfae60ae69b579a840f1e3692abcaee44762
Josnim <103206493+Josnim@users.noreply.github.com>
2022-11-15 01:01:35 -0500

    Add files via upload

---
+++ Prueba1/Prueba1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba1
+{
+    class Program
+    {
+        static int LongestS(string txt)
+        {
+            int max = 0;
+            for (int i=0; i<txt.Length; i++)
+            {
+                string subString = "";
+                for (int j = i; j < txt.Length; j++)
+                {
+                    if (subString.Contains(txt.Substring(j, 1)))
+                    {
+                        break;
+                    }
+                    subString += txt.Substring(j, 1);
+                    if (max < subString.Length)
+                    {
+                        max = subString.Length;
+                    }
+                }
+            }
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(LongestS(input));
+            Console.ReadKey();
+        }
+    }
+}
